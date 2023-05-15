f40ffff5b13a3a8936c61f264de76505440a6eac
michalmielc <102482708+michalmielc@users.noreply.github.com>
2023-05-15 17:10:48 +0200

    0.01 15052023 start projektu

---
+++ app/Http/Controllers/monkey.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class monkey extends Controller
+{
+   
+    
+    public function showMonkey()
+    {
+        return view ("test");
+    }
+
+    
+   
+}
